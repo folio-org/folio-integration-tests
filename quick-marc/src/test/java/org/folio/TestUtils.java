dd14d5eaec72cf2c8755c56acdc047a73e7ae705
siarhei-charniak <siarhei_charniak@epam.com>
2020-08-14 16:11:22 +0300

    MODQM-25 - Karate test cases for quickMARC (#30)

---
+++ quick-marc/src/test/java/org/folio/TestUtils.java
@@ -0,0 +1,16 @@
+package org.folio;
+
+import java.util.Optional;
+import java.util.Random;
+
+public class TestUtils {
+
+  private TestUtils() {
+  }
+
+  public static void runHook() {
+    Optional.ofNullable(System.getenv("karate.env"))
+                          .ifPresent(env -> System.setProperty("karate.env", env));
+    System.setProperty("runId", String.valueOf(new Random().nextInt(100)));
+  }
+}
