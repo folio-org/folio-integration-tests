026f977dc27903ae380b326753da563ff3ae3ed0
Stanislav Miroshnichenko <celt@root.in.ua>
2020-08-20 16:51:43 +0300

    Delete runtests.sh

--- runtests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-_environment="${2:-environment}"
-_project="${1:-project}"
-
-echo "====running tests for $_project in $_environment===="
-mvn test -pl $_project -Dkarate.env=$_environment
