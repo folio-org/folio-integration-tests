33bdb8c55b96dc986afcf841029794e36f9fb86e
Damien <damien-git@users.noreply.github.com>
2021-05-06 10:18:26 -0400

    [EDGORDERS-6] Removed TestRail integration

--- acquisitions/src/test/java/org/folio/EdgeOrdersApiTest.java
+++ acquisitions/src/test/java/org/folio/EdgeOrdersApiTest.java
@@ -1,8 +1,8 @@
 package org.folio;
 
-import org.folio.testrail.AbstractTestRailIntegrationTest;
-import org.folio.testrail.config.TestModuleConfiguration;
-import org.folio.testrail.services.TestRailIntegrationService;
+import org.folio.test.TestBase;
+import org.folio.test.config.TestModuleConfiguration;
+import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -15,18 +15,13 @@ import org.junit.jupiter.api.Test;
  * java -Dport=8000 -Dokapi_url=http://localhost:9130 -Dsecure_store_props=path/to/ephemeral.properties -jar target/edge-orders-fat.jar
  * The port number to use for edge modules is specified in karate-config.js.
  */
-public class EdgeOrdersApiTest extends AbstractTestRailIntegrationTest {
+public class EdgeOrdersApiTest extends TestBase {
 
   // default module settings
   private static final String TEST_BASE_PATH = "classpath:domain/edge-orders/features/";
-  private static final String TEST_SUITE_NAME = "edge-orders";
-  // TEST_SUITE_ID and TEST_SECTION_ID are obtained from TestRail
-  private static final long TEST_SUITE_ID = 1058L;
-  private static final long TEST_SECTION_ID = 15155L;
 
   public EdgeOrdersApiTest() {
-    super(new TestRailIntegrationService(
-        new TestModuleConfiguration(TEST_BASE_PATH, TEST_SUITE_NAME, TEST_SUITE_ID, TEST_SECTION_ID)));
+    super(new TestIntegrationService(new TestModuleConfiguration(TEST_BASE_PATH)));
   }
 
   @Test
