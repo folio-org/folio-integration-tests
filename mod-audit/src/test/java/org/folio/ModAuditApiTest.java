035d7e47000d9873c7fdc616206282e2bd840ebf
Oleksandr Bozhko <Oleksandr_Bozhko@epam.com>
2021-06-30 13:00:08 +0300

    Karate tests for mod-audit, update

--- mod-audit/src/test/java/org/folio/ModAuditApiTest.java
+++ mod-audit/src/test/java/org/folio/ModAuditApiTest.java
@@ -4,9 +4,7 @@ import org.folio.test.TestBase;
 import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ModAuditApiTest extends TestBase {
