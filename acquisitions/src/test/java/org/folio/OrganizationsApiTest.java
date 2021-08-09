9f5b3c0ada7591fc6dc1131b14ee1830eb17be2d
Andrei <40025074+AndreiMPV@users.noreply.github.com>
2021-08-09 15:44:15 +0300

    FAT-24 MODORG: Implement API Karate tests based on Test plans (#233)
    * [FAT-24] Implement karate tests for mod-organizations
    
    * [FAT-24] Implement karate tests for mod-organizations
    
    * [FAT-24] Implement karate tests for mod-organizations
    
    * [FAT-24] Implement karate tests for mod-organizations
    
    * [FAT-24] Implement karate tests for mod-organizations
    
    * [FAT-24] - Fix tests
    
    Co-authored-by: MaksymShtanko <Maksym_Shtanko@epam.com>

---
+++ acquisitions/src/test/java/org/folio/OrganizationsApiTest.java
@@ -0,0 +1,34 @@
+package org.folio;
+
+import org.folio.test.TestBase;
+import org.folio.test.config.TestModuleConfiguration;
+import org.folio.test.services.TestIntegrationService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class OrganizationsApiTest extends TestBase {
+
+    // Default module settings :
+    private static final String TEST_BASE_PATH = "classpath:domain/mod-organizations/features/";
+
+    public OrganizationsApiTest() {
+        super(new TestIntegrationService(new TestModuleConfiguration(TEST_BASE_PATH)));
+    }
+
+    @BeforeAll
+    public void organizationsApiTestBeforeAll() {
+        runFeature("classpath:domain/mod-organizations/organizations-junit.feature");
+    }
+
+    @AfterAll
+    public void organizationsApiTestAfterAll() {
+        runFeature("classpath:common/destroy-data.feature");
+    }
+
+    // Feature(s) list :
+    @Test
+    public void createTestDataFeature() {
+        runFeature("create-test-data.feature");
+    }
+}
