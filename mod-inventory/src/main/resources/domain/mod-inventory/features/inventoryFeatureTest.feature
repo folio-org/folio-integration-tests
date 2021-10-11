ea681782c5e4f272090271062380b052aa50d17d
SinghAdes <Adesh_Singh@epam.com>
2021-10-11 11:29:39 +0530

    setup done

---
+++ mod-inventory/src/main/resources/domain/mod-inventory/features/inventoryFeatureTest.feature
@@ -0,0 +1,12 @@
+Feature: inventory sample
+
+  Background:
+    * url baseUrl
+    * callonce login testUser
+    * configure headers = { 'Content-Type': 'application/json', 'x-okapi-token': '#(okapitoken)', 'Accept': 'application/json, text/plain' }
+    
+
+  Scenario: print
+    * print 'Hello Karate-setup'
+
+
---
+++ mod-inventory/src/test/java/org/folio/ModInventoryTests.java
@@ -0,0 +1,32 @@
+package org.folio;
+
+import net.minidev.json.JSONUtil;
+import org.folio.test.TestBase;
+import org.folio.test.config.TestModuleConfiguration;
+import org.folio.test.services.TestIntegrationService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ModInventoryTests extends TestBase {
+  private static final String TEST_BASE_PATH = "classpath:domain/mod-inventory/features/";
+
+  public ModInventoryTests() {
+    super(new TestIntegrationService(new TestModuleConfiguration(TEST_BASE_PATH)));
+  }
+
+  @BeforeAll
+  public void setup() {
+    runFeature("classpath:domain/mod-inventory/inventory-junit.feature");
+  }
+
+  @AfterAll
+  public void tearDown() {
+    runFeature("classpath:common/destroy-data.feature");
+  }
+
+  @Test
+  void calendarPeriodsTest() {
+    runFeatureTest("inventoryFeatureTest");
+  }
+}
