07c09f6b5bf82d1c25e4079c3e64abb00b9bf5cf
SinghAdes <Adesh_Singh@epam.com>
2021-10-18 21:58:48 +0530

    karate test for creating new instance.

--- mod-inventory/src/main/resources/karate-config.js
+++ mod-inventory/src/main/resources/karate-config.js
@@ -63,7 +63,7 @@ function fn() {
     config.baseUrl = 'https://folio-snapshot-okapi.dev.folio.org:443';
     config.admin = {
       tenant: 'supertenant',
-      name: 'test_admin',
+      name: 'testing_admin',
       password: 'admin'
     }
   } else if (env != null && env.match(/^ec2-\d+/)) {
