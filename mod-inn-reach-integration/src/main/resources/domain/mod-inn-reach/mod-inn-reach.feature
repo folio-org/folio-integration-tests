1ed1f62f73331a7b8d4ce19b9fa971bd85605833
Oleksandr_Oliinyk2 <oleksandr_oliinyk2@epam.com>
2021-12-29 16:30:46 +0200

    mod-inn-reach: Setup API Karate tests in the module

---
+++ mod-inn-reach-integration/src/main/resources/domain/mod-inn-reach/mod-inn-reach.feature
@@ -0,0 +1,35 @@
+Feature: mod-inn-reach integration tests
+
+  Background:
+    * url baseUrl
+    * table modules
+      | name                |
+      | 'mod-inn-reach'     |
+      | 'mod-login'         |
+      | 'mod-permissions'   |
+      | 'mod-configuration' |
+      | 'mod-users'         |
+
+    * def random = callonce randomMillis
+    * def testTenant = 'test_inn-reach' + random
+    #* def testTenant = 'test_inn_reach7'
+    # -Dkarate.env=testing
+    * def testAdmin = {tenant: '#(testTenant)', name: 'test-admin', password: 'admin'}
+    * def testUser = {tenant: '#(testTenant)', name: 'test-user', password: 'test'}
+    * def testSystemUser = { tenant: '#(testTenant)', name: 'test-system-user', password: 'system-test' }
+
+    * table adminAdditionalPermissions
+      | name                                                                 |
+      | 'inn-reach.central-servers.collection.get'                           |
+
+
+    * table userPermissions
+      | name                                                                 |
+      | 'inn-reach.central-servers.collection.get'                           |
+
+  Scenario: create tenant and users for testing for mod-inn-reach
+    Given call read('classpath:common/setup-users.feature')
+
+
+  Scenario: wipe data
+    Given call read('classpath:common/destroy-data.feature')
---
+++ mod-inn-reach-integration/src/main/resources/karate-config.js
@@ -0,0 +1,110 @@
+function fn() {
+
+  karate.configure('logPrettyRequest', true);
+  karate.configure('logPrettyResponse', true);
+
+  var env = karate.env;
+
+  // The "testTenant" property could be specified during test runs
+  var testTenant = karate.properties['testTenant'];
+  // the token property
+
+  var config = {
+    baseUrl: 'http://localhost:9130',
+    edgeUrl: 'http://localhost:8000',
+    admin: {tenant: 'diku', name: 'diku_admin', password: 'admin'},
+
+    testTenant: testTenant ? testTenant: 'testTenant',
+    testAdmin: {tenant: testTenant, name: 'test-admin', password: 'admin'},
+    testUser: {tenant: testTenant, name: 'test-user', password: 'test'},
+
+    // define global features
+    login: karate.read('classpath:common/login.feature'),
+    loginRegularUser: karate.read('classpath:common/login.feature'),
+    loginAdmin: karate.read('classpath:common/login.feature'),
+    dev: karate.read('classpath:common/dev.feature'),
+
+    // define global functions
+    uuid: function () {
+      return java.util.UUID.randomUUID() + ''
+    },
+
+    random: function (max) {
+      return Math.floor(Math.random() * max)
+    },
+
+    randomMillis: function() {
+      return java.lang.System.currentTimeMillis() + '';
+    },
+
+    random_string: function() {
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+      for (var i = 0; i < 5; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      return text;
+    },
+    getCurrentYear: function() {
+      var SimpleDateFormat = Java.type('java.text.SimpleDateFormat');
+      var sdf = new SimpleDateFormat('yyyy');
+      var date = new java.util.Date();
+      return sdf.format(date);
+    },
+    getCurrentDate: function() {
+      var SimpleDateFormat = Java.type('java.text.SimpleDateFormat');
+      var sdf = new SimpleDateFormat('yyyy-MM-dd');
+      var date = new java.util.Date();
+      return sdf.format(date);
+    },
+
+    getYesterday: function() {
+      var LocalDate = Java.type('java.time.LocalDate');
+      var localDate = LocalDate.now().minusDays(1);
+      var formatter = java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd");
+      var formattedString = localDate.format(formatter);
+      return localDate.format(formatter);
+    },
+
+    pause: function(millis) {
+      var Thread = Java.type('java.lang.Thread');
+      Thread.sleep(millis);
+    }
+
+  };
+
+  // Create 100 functions for uuid generation
+  var rand = function(i) {
+    karate.set("uuid"+i, function() {
+      return java.util.UUID.randomUUID() + '';
+    });
+  }
+  karate.repeat(100, rand);
+
+  if (env == 'testing') {
+    config.baseUrl = 'https://volaris-okapi.ci.folio.org:443';
+    config.edgeUrl = 'https://volaris.ci.folio.org:8000';
+    config.admin = {
+      tenant: 'supertenant',
+      name: 'testing_admin',
+      password: 'admin'
+    }
+  } else if (env == 'snapshot') {
+    config.baseUrl = 'https://folio-snapshot-okapi.dev.folio.org:443';
+    config.edgeUrl = 'https://folio-snapshot.dev.folio.org:8000';
+    config.admin = {
+      tenant: 'supertenant',
+      name: 'testing_admin',
+      password: 'admin'
+    }
+  } else if (env != null && env.match(/^ec2-\d+/)) {
+    // Config for FOLIO CI "folio-integration" public ec2- dns name
+    config.baseUrl = 'http://' + env + ':9130';
+    config.edgeUrl = 'http://' + env + ':8000';
+    config.admin = {
+      tenant: 'supertenant',
+      name: 'admin',
+      password: 'admin'
+    }
+  }
+  return config;
+}
