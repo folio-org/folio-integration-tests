6880eec803662169186f9043d0a686f92f105ca0
Aleh_Liameshka <aleh_liameshka@epam.com>
2022-03-28 11:31:23 +0300

    FAT-1513 rename function

--- data-import/src/test/java/org/folio/DataImportApiTest.java
+++ data-import/src/test/java/org/folio/DataImportApiTest.java
@@ -26,7 +26,7 @@ public class DataImportApiTest extends TestBase {
 
     @Test
     @Order(2)
-    void createHoldingsRecordsTest() {
+    void createMarcRecordsTest() {
         runFeatureTest("create-marc-records");
     }
 
