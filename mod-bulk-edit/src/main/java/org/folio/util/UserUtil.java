4c8debeb6a807da6b592bc06f3ba7342a8b1a6e8
Illia Daliek <60663044+IlliaDaliek@users.noreply.github.com>
2022-04-12 13:03:41 +0300

    FAT-1592: implement Bulk-Edit (Users) API tests (#520)

---
+++ mod-bulk-edit/src/main/java/org/folio/util/UserUtil.java
@@ -0,0 +1,61 @@
+package org.folio.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserUtil {
+
+    private static final int CREATED_DATE_INDEX = 21;
+    private static final int UPDATED_DATE_INDEX = 22;
+
+    /**
+     * Compares user csv file content without taking into account the user CreatedDate and UpdateDate fields
+     * since the expected csv file is hardcoded and those fields will not be matching
+     *
+     * @param expectedCsv - file system expectedCsv file
+     * @param obtainedCsv - obtainedCsv file from bulk-edit API
+     */
+    public boolean compareUsersCsvFilesString(String expectedCsv, String obtainedCsv) {
+        String editedExpectedCsv = replaceCreatedAndUpdatedDatesWithEmptyStrings(expectedCsv, true);
+        String editedObtainedCsv = replaceCreatedAndUpdatedDatesWithEmptyStrings(obtainedCsv, false);
+        if (editedExpectedCsv.equals(editedObtainedCsv)) {
+            System.out.println("strings are equal");
+            return true;
+        }
+        System.out.println("strings are not equal");
+        return false;
+    }
+
+    private String replaceCreatedAndUpdatedDatesWithEmptyStrings(String csvStringToModify, boolean shouldUseSystemLineSeparator) {
+        String lineSeparator = shouldUseSystemLineSeparator ? System.lineSeparator() : "\\n";
+        List<String> csvStrings = Arrays.asList(csvStringToModify.split(lineSeparator));
+        List<String> editedCsvStrings = new ArrayList<>();
+        editedCsvStrings.add(csvStrings.get(0));
+        csvStrings.stream()
+                .skip(1)
+                .map(csvString -> {
+                    StringBuilder editedCsvString = new StringBuilder();
+                    List<String> csvStringValues = Arrays.asList(csvString.split(","));
+                    csvStringValues.set(UPDATED_DATE_INDEX, "");
+                    csvStringValues.set(CREATED_DATE_INDEX, "");
+                    csvStringValues.forEach(csvColumnValue -> editedCsvString.append(csvColumnValue)
+                            .append(","));
+                    return editedCsvString.toString();
+                })
+                .collect(Collectors.toCollection(() -> editedCsvStrings));
+
+        StringBuilder modifiedCsvString = new StringBuilder();
+        editedCsvStrings.forEach(str -> modifiedCsvString.append(str)
+                .append(System.lineSeparator()));
+        return modifiedCsvString.toString();
+    }
+
+    public boolean compareErrorsCsvFiles(String expectedCsv, String obtainedCsv) {
+        String expectedErrorsCsv = String.join(",", Arrays.asList(expectedCsv.split(System.lineSeparator())));
+        String obtainedErrorsCsv = String.join(",", Arrays.asList(obtainedCsv.split("\\n")));
+        return expectedErrorsCsv.equals(obtainedErrorsCsv);
+    }
+
+}
---
+++ mod-bulk-edit/src/main/resources/global/mod_users_init_data.feature
@@ -0,0 +1,42 @@
+Feature: init data for mod-users
+
+  Background:
+    * url baseUrl
+
+    * callonce login testUser
+    * def okapitokenAdmin = okapitoken
+
+    * configure headers = { 'Content-Type': 'application/json', 'Accept': 'application/json', 'x-okapi-token': '#(okapitokenAdmin)' }
+    * configure retry = { interval: 3000, count: 10 }
+
+  Scenario: setup test data
+    #setup address types
+    * def addressTypes = karate.read('classpath:samples/user/addressTypes.json')
+    * def fun = function(i) { return { addressType: addressTypes[i]}; }
+    * def data = karate.repeat(2, fun)
+    * call read('classpath:global/util/mod-users-util.feature@PostAddressType') data
+
+    #setup departments
+    * def departments = karate.read('classpath:samples/user/departments.json')
+    * def fun = function(i) { return { department: departments[i]}; }
+    * def data = karate.repeat(2, fun)
+    * call read('classpath:global/util/mod-users-util.feature@PostDepartment') data
+
+    #setup patron groups
+    * def groups = karate.read('classpath:samples/user/patronGroups.json')
+    * def fun = function(i) { return { group: groups[i]}; }
+    * def data = karate.repeat(2, fun)
+    * call read('classpath:global/util/mod-users-util.feature@PostPatronGroup') data
+
+    #setup proxies for
+    * def proxiesForJson = karate.read('classpath:samples/user/proxiesFor.json')
+    * def proxiesFor = karate.append(proxiesForJson.original, proxiesForJson.changed)
+    * def fun = function(i) { return { proxyFor: proxiesFor[i]}; }
+    * def data = karate.repeat(6, fun)
+    * call read('classpath:global/util/mod-users-util.feature@PostProxiesFor') data
+
+    #setup test users
+    * def users = karate.append(usersDataOriginal.normalUsers, usersDataOriginal.proxyUsers)
+    * def fun = function(i) { return { user: users[i]}; }
+    * def data = karate.repeat(5, fun)
+    * call read('classpath:global/util/mod-users-util.feature@PostUser') data
