abfe83a1412d270c2e3c06acdd698f910c3ed1ee
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2022-05-09 16:41:55 +0300

    draft

--- mod-inn-reach/src/main/resources/karate-config.js
+++ mod-inn-reach/src/main/resources/karate-config.js
@@ -7,7 +7,6 @@ function fn() {
 
   // The "testTenant" property could be specified during test runs
   var testTenant = karate.properties['testTenant'];
-  // the token property
 
   var config = {
     baseUrl: 'http://localhost:9130',
@@ -23,6 +22,7 @@ function fn() {
     loginRegularUser: karate.read('classpath:common/login.feature'),
     loginAdmin: karate.read('classpath:common/login.feature'),
     dev: karate.read('classpath:common/dev.feature'),
+    variables: karate.read('classpath:volaris/mod-inn-reach/global/variables.feature'),
 
     // define global functions
     uuid: function () {
@@ -96,6 +96,14 @@ function fn() {
       name: 'testing_admin',
       password: 'admin'
     }
+  } else if (env == 'rancher') {
+        config.baseUrl = 'https://volaris-okapi.ci.folio.org/';
+        config.edgeUrl = 'https://volaris-edge-inn-reach.ci.folio.org';
+        config.admin = {
+          tenant: 'diku',
+          name: 'diku_admin',
+          password: 'admin'
+        }
   } else if (env != null && env.match(/^ec2-\d+/)) {
     // Config for FOLIO CI "folio-integration" public ec2- dns name
     config.baseUrl = 'http://' + env + ':9130';
@@ -106,5 +114,9 @@ function fn() {
       password: 'admin'
     }
   }
+
+  //   uncomment to run on local
+    karate.callSingle('classpath:volaris/mod-inn-reach/global/add-okapi-permissions.feature', config);
+
   return config;
 }
