8c0d4d7f80c0cda73deeba66396a2950def78a9e
Vitaly Demchenko <Vitaly_Demchenko@epam.com>
2022-05-31 17:43:13 +0300

    RANCHER-252 disable edge modules

--- edge-patron/src/test/java/org/folio/EdgePatronTests.java
+++ edge-patron/src/test/java/org/folio/EdgePatronTests.java
@@ -4,14 +4,15 @@ import org.folio.test.TestBase;
 import org.folio.test.annotation.FolioTest;
 import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+@Disabled
 @FolioTest(team = "prokopovych", module = "edge-patron")
-class EdgepatronTests extends TestBase {
+class EdgePatronTests extends TestBase {
 
-  public EdgepatronTests() {
+  public EdgePatronTests() {
     super(new TestIntegrationService(new TestModuleConfiguration("classpath:prokopovych/edge-patron/features/")));
   }
 
