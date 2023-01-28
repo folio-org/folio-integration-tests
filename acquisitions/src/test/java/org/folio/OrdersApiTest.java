b66e23bb0ecdfcfdda16a9b7193f51540f6c656c
Abdulkhakimov <89521577+Abdulkhakimov@users.noreply.github.com>
2023-01-28 20:47:51 +0500

    [MODORDERS-834] - Flow #2. Copy the amount awaiting payment and expended from the original encumbrance (#844)
    * [MODORDERS-834] Removed unused scenario
    * [MODORDERS-834] Implemented new test scenario
    Co-authored-by: Serhii Nosko <Serhii_Nosko@epam.com>

--- acquisitions/src/test/java/org/folio/OrdersApiTest.java
+++ acquisitions/src/test/java/org/folio/OrdersApiTest.java
@@ -234,6 +234,11 @@ public class OrdersApiTest extends TestBase {
   }
 
   @Test
+  void movingExpendedValueToNewlyCreatedEncumbrance() {
+    runFeatureTest("moving_expended_value_to_newly_created_encumbrance");
+  }
+
+  @Test
   void updateLinkedInvoiceLinesFundDistributionReference() {
     runFeatureTest("update_linked_invoice_lines_fund_distribution_reference");
   }
