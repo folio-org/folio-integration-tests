149a547350ba4f89d9cc65d04e86c5e507878349
RomanChernetskyi <111740564+RomanChernetskyi@users.noreply.github.com>
2023-01-30 10:01:23 +0200

    [FAT-3436] Add Karate tests to cover CopyCatProfiles API (#843)

---
+++ mod-copycat/src/test/java/org/folio/ModCopycatTest.java
@@ -0,0 +1,34 @@
+package org.folio;
+
+import org.folio.test.TestBase;
+import org.folio.test.annotation.FolioTest;
+import org.folio.test.config.TestModuleConfiguration;
+import org.folio.test.services.TestIntegrationService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+@FolioTest(team = "folijet", module = "mod-copycat")
+public class ModCopycatTest extends TestBase {
+    private static final String TEST_BASE_PATH = "classpath:folijet/mod-copycat/features/";
+
+    public ModCopycatTest() {
+        super(new TestIntegrationService(new TestModuleConfiguration(TEST_BASE_PATH)));
+    }
+
+    @BeforeAll
+    public void setup() {
+        runFeature("classpath:folijet/mod-copycat/copycat-junit.feature");
+    }
+
+    @AfterAll
+    public void tearDown() {
+        runFeature("classpath:common/destroy-data.feature");
+    }
+
+    @Test
+    void getMappingRulesTest() {
+        runFeatureTest("copycat-profiles.feature");
+    }
+
+}
