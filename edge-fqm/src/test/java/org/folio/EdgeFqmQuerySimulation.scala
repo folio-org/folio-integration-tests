0757c234d959009260dbee808067b112391f6d3c
Noah Overcash <novercash@ebsco.com>
2024-06-13 12:49:17 -0400

    edge-fqm gatling tests

---
+++ edge-fqm/src/test/java/org/folio/EdgeFqmQuerySimulation.scala
@@ -0,0 +1,46 @@
+package org.folio
+
+import com.intuit.karate.gatling.PreDef._
+import io.gatling.core.Predef._
+import org.apache.commons.lang3.RandomUtils
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+class EdgeFqmQuerySimulation extends Simulation {
+
+  def generateTenantId(): String = {
+    val constantString = "testtenant"
+    val randomLong = RandomUtils.nextLong
+    constantString + randomLong
+  }
+
+  val protocol = karateProtocol(
+    "/_/proxy/tenants/{tenant}" -> Nil,
+    "/_/proxy/tenants/{tenant}/modules" -> Nil,
+    "/_/proxy/tenants/{tenant}/install" -> Nil,
+    "/query" -> Nil,
+    "/query/{queryId}" -> Nil,
+    "/query/purge" -> Nil,
+    "/entity-types" -> Nil,
+    "/entity-types/{entityTypeId}" -> Nil,
+    "/entity-types/{entityTypeId}/columns/{columnName}/values" -> Nil,
+    "/users/{userId}" -> Nil,
+  )
+  protocol.runner.systemProperty("testTenant", generateTenantId())
+
+  val before = scenario("before")
+    .exec(karateFeature("classpath:corsair/edge-fqm/edge-fqm-junit.feature"))
+  val query = scenario("query")
+    .repeat(10) {
+      exec(karateFeature("classpath:corsair/edge-fqm/features/edge-query.feature"))
+    }
+  val after = scenario("after").exec(karateFeature("classpath:common/destroy-data.feature"))
+
+  setUp(
+    before.inject(atOnceUsers(1))
+      .andThen(query.inject(nothingFor(3 seconds), rampUsers(3) during (5 seconds)))
+      .andThen(after.inject(nothingFor(3 seconds), atOnceUsers(1))),
+  ).protocols(protocol)
+
+}
