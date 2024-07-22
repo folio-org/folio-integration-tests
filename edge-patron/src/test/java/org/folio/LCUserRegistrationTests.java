d9e532a99454f232ed3f92da3344d67e400cdcff
Kapil Verma <85541312+kapil-epam@users.noreply.github.com>
2024-07-22 18:16:11 +0530

    FAT-14153: adding karate tests for LC user registration feature (#1463)
    * FAT-14153: adding karate tests for LC user registration feature
    
    * FAT-14153: commented Get LC users test case until next PR
    
    * FAT-14153: reverted Get LC users test case
    
    ---------
    
    Co-authored-by: Kapil Verma <kverma@ebsco.com>

---
+++ edge-patron/src/test/java/org/folio/LCUserRegistrationTests.java
@@ -0,0 +1,27 @@
+package org.folio;
+
+import org.folio.test.TestBase;
+import org.folio.test.annotation.FolioTest;
+import org.folio.test.config.TestModuleConfiguration;
+import org.folio.test.services.TestIntegrationService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+@FolioTest(team = "vega", module = "edge-patron")
+class LCUserRegistrationTests extends TestBase {
+
+  public LCUserRegistrationTests() {
+    super(new TestIntegrationService(new TestModuleConfiguration("classpath:vega/edge-patron/features/")));
+  }
+
+  @BeforeAll
+  public void setup() {
+    runFeature("classpath:vega/edge-patron/patron-junit.feature");
+  }
+
+  @Test
+  void patronTest() {
+    runFeatureTest("lc-user-registration");
+  }
+}
