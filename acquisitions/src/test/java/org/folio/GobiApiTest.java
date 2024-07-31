14a3137c2152816b20971d6ba703b453cc60da19
Azizbek Khushvakov <azizbek_khushvakov@epam.com>
2024-07-31 16:12:18 +0500

    Revert back gobi test

---
+++ acquisitions/src/test/java/org/folio/GobiApiTest.java
@@ -0,0 +1,38 @@
+package org.folio;
+
+import org.folio.test.TestBase;
+import org.folio.test.annotation.FolioTest;
+import org.folio.test.config.TestModuleConfiguration;
+import org.folio.test.services.TestIntegrationService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+@FolioTest(team = "thunderjet", module = "mod-gobi")
+public class GobiApiTest extends TestBase{
+
+    // Default module settings :
+    private static final String TEST_BASE_PATH = "classpath:thunderjet/mod-gobi/features/";
+
+    public GobiApiTest() {
+        super(new TestIntegrationService(new TestModuleConfiguration(TEST_BASE_PATH)));
+    }
+
+    @BeforeAll
+    public void gobiApiTestBeforeAll() {
+    runFeature("classpath:thunderjet/mod-gobi/gobi-junit.feature");
+    }
+
+    @AfterAll
+    public void financeApiTestAfterAll() {
+    runFeature("classpath:common/destroy-data.feature");
+}
+    // Feature(s) list :
+    @Test
+    void gobiApiTest() {
+    runFeature("classpath:thunderjet/mod-gobi/gobi.feature");
+    }
+
+}
+
+
