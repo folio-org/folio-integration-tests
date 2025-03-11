ee24a3763ce0a99d68dab15c70698050814d45d9
viktor-draban <Viktor_Draban@epam.com>
2025-03-11 10:34:15 +0200

    add vital fields for mod-batch-print in karate-config.js

--- mod-batch-print/src/main/resources/karate-config.js
+++ mod-batch-print/src/main/resources/karate-config.js
@@ -84,6 +84,8 @@ function fn() {
     }
     config.prototypeTenant = '${prototypeTenant}';
     karate.configure('ssl',true);
+    config.baseKeycloakUrl = 'https://folio-etesting-karate-eureka-keycloak.ci.folio.org';
+    config.clientSecret = karate.properties['clientSecret'] || 'SecretPassword';
   } else if (env != null && env.match(/^ec2-\d+/)) {
     // Config for FOLIO CI "folio-integration" public ec2- dns name
     config.baseUrl = 'http://' + env + ':9130';
