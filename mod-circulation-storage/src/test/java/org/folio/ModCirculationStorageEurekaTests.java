96591cb856ad1a180cd9334bb9e432e781303154
[200~2110107@newuu.uz <2110107@newuu.uz>
2025-03-24 13:11:43 +0500

    fix bug

--- mod-circulation-storage/src/test/java/org/folio/ModCirculationStorageEurekaTests.java
+++ mod-circulation-storage/src/test/java/org/folio/ModCirculationStorageEurekaTests.java
@@ -6,9 +6,11 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "vega", module = "mod-circulation-storage")
+@Disabled
 class ModCirculationStorageEurekaTests extends TestBaseEureka {
 
     private static final String TEST_BASE_PATH = "classpath:vega/mod-circulation-storage/eureka-features/";
@@ -31,10 +33,12 @@ class ModCirculationStorageEurekaTests extends TestBaseEureka {
     void rootTest() {
         runFeatureTest("root");
     }
+
     @Test
     void printEventsTest() {
         runFeatureTest("print-events");
     }
+
     @Test
     void circulationSettingsTest() {
         runFeatureTest("circulation-settings");
