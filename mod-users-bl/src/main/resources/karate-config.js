c19c606a7d7054aad9f5e2df81d2cc5ee961b372
TolKing1 <tolking0101@gmail.com>
2025-03-25 17:26:08 +0500

    feat: refactored "mod-user-bl" module against Eureka

--- mod-users-bl/src/main/resources/karate-config.js
+++ mod-users-bl/src/main/resources/karate-config.js
@@ -10,6 +10,7 @@ function fn() {
 
   // The "testTenant" property could be specified during test runs
   var testTenant = karate.properties['testTenant'] || 'testtenant';
+  var testTenantId = karate.properties['testTenantId'];
 
   var config = {
     baseUrl: 'http://localhost:9130',
@@ -17,6 +18,7 @@ function fn() {
     prototypeTenant: 'diku',
 
     testTenant: testTenant,
+    testTenantId: testTenantId ? testTenantId : (function() { return java.util.UUID.randomUUID() + '' })(),
     testAdmin: {tenant: testTenant, name: 'test-admin', password: 'admin'},
     // The tenant property of testUser is required by destroy-data.feature
     testUser: {tenant: testTenant, name: 'test-user', password: 'test'},
@@ -91,6 +93,10 @@ function fn() {
       name: 'testing_admin',
       password: 'admin'
     }
+  } else if (env == 'eureka') {
+     config.baseUrl = 'https://folio-edev-dojo-kong.ci.folio.org:443';
+     config.baseKeycloakUrl = 'https://folio-edev-dojo-keycloak.ci.folio.org:443';
+     config.clientSecret = karate.properties['clientSecret'];
   } else if(env == 'folio-testing-karate') {
     config.baseUrl = '${baseUrl}';
     config.admin = {
@@ -100,6 +106,8 @@ function fn() {
     }
     config.prototypeTenant = '${prototypeTenant}';
     karate.configure('ssl',true);
+    config.baseKeycloakUrl = 'https://folio-etesting-karate-eureka-keycloak.ci.folio.org';
+    config.clientSecret = karate.properties['clientSecret'] || 'SecretPassword';
   } else if (env != null && env.match(/^ec2-\d+/)) {
     // Config for FOLIO CI "folio-integration" public ec2- dns name
     config.baseUrl = 'http://' + env + ':9130';
