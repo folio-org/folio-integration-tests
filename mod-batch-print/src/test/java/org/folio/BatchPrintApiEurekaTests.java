9f58ff03ef00b557a3c9134b2a142151de6e1ade
TolKing1 <tolking0101@gmail.com>
2025-03-26 18:42:45 +0500

    Revert "temp: temporary disable tests"
    This reverts commit 0243e699ba2d74a4dc585e613a5a40ba826addae.

--- mod-batch-print/src/test/java/org/folio/BatchPrintApiEurekaTests.java
+++ mod-batch-print/src/test/java/org/folio/BatchPrintApiEurekaTests.java
@@ -6,11 +6,9 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "Odin", module = "mod-batch-print")
-@Disabled("REMOVE AFTER THE TESTS")
 class BatchPrintApiEurekaTests extends TestBaseEureka {
 
     private static final String TEST_BASE_PATH = "classpath:odin/mod-batch-print/eureka-features/";
