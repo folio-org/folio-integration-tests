ce1cf1929b7d82a63b76e05f78a9409a79113b6d
viktor-draban <Viktor_Draban@epam.com>
2025-03-26 19:54:20 +0200

    expanded imports

--- data-import/src/test/java/org/folio/DataImportApiEurekaTest.java
+++ data-import/src/test/java/org/folio/DataImportApiEurekaTest.java
@@ -4,7 +4,12 @@ import org.folio.test.TestBaseEureka;
 import org.folio.test.annotation.FolioTest;
 import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @FolioTest(team = "folijet", module = "data-import")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
