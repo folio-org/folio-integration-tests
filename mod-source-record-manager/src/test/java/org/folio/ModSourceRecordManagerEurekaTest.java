3ff5d68e3b4264c9d02440824bd989788e616c47
TolKing1 <tolking0101@gmail.com>
2025-03-27 17:50:26 +0500

    Revert "temp: temporary disable tests"
    This reverts commit 0243e699ba2d74a4dc585e613a5a40ba826addae.

--- mod-source-record-manager/src/test/java/org/folio/ModSourceRecordManagerEurekaTest.java
+++ mod-source-record-manager/src/test/java/org/folio/ModSourceRecordManagerEurekaTest.java
@@ -6,11 +6,9 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "folijet", module = "mod-source-record-manager")
-@Disabled("REMOVE AFTER THE TESTS")
 public class ModSourceRecordManagerEurekaTest extends TestBaseEureka {
   private static final String TEST_BASE_PATH = "classpath:folijet/mod-source-record-manager/eureka-features/";
 
