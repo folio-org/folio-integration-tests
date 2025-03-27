0c615920d5fe59482efae01ad69d2767b5f566a0
TolKing1 <tolking0101@gmail.com>
2025-03-27 17:15:37 +0500

    disable: disable tests for Spitfire team due to migration from Okapi to Eureka

--- mod-entities-links/src/test/java/org/folio/ModEntitiesLinksTest.java
+++ mod-entities-links/src/test/java/org/folio/ModEntitiesLinksTest.java
@@ -6,9 +6,11 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "spitfire", module = "mod-entities-links")
+@Disabled("Migrated to Eureka")
 class ModEntitiesLinksTest extends TestBase {
     private static final String TEST_BASE_PATH = "classpath:spitfire/mod-entities-links/features/";
 
