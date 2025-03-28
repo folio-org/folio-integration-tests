e21e6ceffc2a64727732b8063d3f78e18c46f910
Maksim_Pokhodin <Maksim_Pokhodin@epam.com>
2025-03-28 19:16:53 +0500

    in progress

--- acquisitions/src/main/resources/karate-config.js
+++ acquisitions/src/main/resources/karate-config.js
@@ -309,7 +309,9 @@ function fn() {
       password: 'admin'
     }
   } else if(env == 'folio-testing-karate') {
-    config.baseUrl = '${baseUrl}';
+    config.baseUrl = 'https://folio-edev-dojo-kong.ci.folio.org:443';
+    config.baseKeycloakUrl = 'https://folio-edev-dojo-keycloak.ci.folio.org:443';
+    config.clientSecret = karate.properties['clientSecret'];
     config.edgeUrl = '${edgeUrl}';
     config.ftpUrl = 'ftp://ftp.ci.folio.org';
     config.ftpPort = 21;
