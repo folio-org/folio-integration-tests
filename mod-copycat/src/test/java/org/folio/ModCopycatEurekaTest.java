6f71b94c12da9252c1daf5e19aebf8a876f65f09
TolKing1 <tolking0101@gmail.com>
2025-03-28 17:00:22 +0500

    Revert "temp: disable unnecessary tests"
    This reverts commit 3a0c864782e9b378c79d2a4efd9376d5a24320eb.

--- mod-copycat/src/test/java/org/folio/ModCopycatEurekaTest.java
+++ mod-copycat/src/test/java/org/folio/ModCopycatEurekaTest.java
@@ -7,11 +7,9 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "folijet", module = "mod-copycat")
-@Disabled("REMOVE AFTER THE TEST")
 public class ModCopycatEurekaTest extends TestBaseEureka {
     private static final String TEST_BASE_PATH = "classpath:folijet/mod-copycat/eureka-features/";
 
--- mod-feesfines/src/test/java/org/folio/FeesFinesApiEurekaTests.java
+++ mod-feesfines/src/test/java/org/folio/FeesFinesApiEurekaTests.java
@@ -6,11 +6,9 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "vega", module = "mod-feesfines")
-@Disabled("REMOVE AFTER THE TEST")
 class FeesFinesApiEurekaTests extends TestBaseEureka {
 
     private static final String TEST_BASE_PATH = "classpath:vega/mod-feesfines/eureka-features/";
