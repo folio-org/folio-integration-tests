fb0306e49b75e8dc8c7736113fec90749b249382
SvitlanaKovalova1 <Svitlana_Kovalova1@epam.com>
2025-03-31 11:43:04 +0300

    FAT-17477: Implement Karate tests for FOLIO instance/FOLIO holdings/FOLIO item - Audit History (#1808)
    (cherry picked from commit d08e212e85380ed700d489d9848ecaac31dd36e5)

--- mod-audit/src/test/java/org/folio/ModAuditApiTest.java
+++ mod-audit/src/test/java/org/folio/ModAuditApiTest.java
@@ -17,7 +17,7 @@ public class ModAuditApiTest extends TestBase {
 
   public ModAuditApiTest() {
     super(new TestIntegrationService(
-      new TestModuleConfiguration(TEST_BASE_PATH)));
+        new TestModuleConfiguration(TEST_BASE_PATH)));
   }
 
   @BeforeAll
@@ -29,7 +29,7 @@ public class ModAuditApiTest extends TestBase {
   public void tearDown() {
     runFeature("classpath:common/destroy-data.feature");
   }
-  
+
   @Test
   void loanEventTests() {
     runFeatureTest("loanEvent");
@@ -39,7 +39,7 @@ public class ModAuditApiTest extends TestBase {
   void requestEventTests() {
     runFeatureTest("requestEvent");
   }
-  
+
   @Test
   void checkInCheckOutTests() {
     runFeatureTest("checkInCheckOutEvent");
@@ -48,5 +48,20 @@ public class ModAuditApiTest extends TestBase {
   @Test
   void marcAuditDataTests() {
     runFeatureTest("marcAuditData");
+  }
+
+  @Test
+  void instanceAuditDataTests() {
+    runFeatureTest("instanceAuditData");
+  }
+
+  @Test
+  void holdingAuditDataTests() {
+    runFeatureTest("holdingAuditData");
+  }
+
+  @Test
+  void itemAuditDataTests() {
+    runFeatureTest("itemAuditData");
   }
 }
