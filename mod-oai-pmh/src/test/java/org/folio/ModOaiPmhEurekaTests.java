5e8a45d67f27a8528d5b9bc931e18e048726d44c
[200~2110107@newuu.uz <2110107@newuu.uz>
2025-04-03 12:16:18 +0500

    feat(tests): modified

--- mod-oai-pmh/src/test/java/org/folio/ModOaiPmhEurekaTests.java
+++ mod-oai-pmh/src/test/java/org/folio/ModOaiPmhEurekaTests.java
@@ -4,13 +4,14 @@ import org.folio.test.TestBaseEureka;
 import org.folio.test.annotation.FolioTest;
 import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
 @FolioTest(team = "firebird", module = "mod-oai-pmh")
 public class ModOaiPmhEurekaTests extends TestBaseEureka {
-    private static final String TEST_BASE_PATH = "classpath:firebird/oaipmh-eureka/";
+    private static final String TEST_BASE_PATH = "classpath:firebird/oaipmh/eureka";
 
     public ModOaiPmhEurekaTests() {
         super(new TestIntegrationService(
@@ -22,13 +23,38 @@ public class ModOaiPmhEurekaTests extends TestBaseEureka {
         runFeature("classpath:firebird/mod-oai-pmh-junit-eureka.feature");
     }
 
-//    @AfterAll
-//    public void tearDown() {
-//        runFeature("classpath:common/eureka/destroy-data.feature");
-//    }
+    @AfterAll
+    public void tearDown() {
+        runFeature("classpath:common/eureka/destroy-data.feature");
+    }
 
     @Test
     void quickExportTest() {
         runFeatureTest("oaipmh-enhancement");
+    }
+
+    @Test
+    void oaiPmhListRecordsAdditionalTests() {
+        runFeatureTest("oaipmh-listRecords-additional");
+    }
+
+    @Test
+    void oaiPmhListRecordsAdditionalTestsWhenSourceIsInventory() {
+        runFeatureTest("oaipmh-listRecords-sourceInventory");
+    }
+
+    @Test
+    void oaiPmhMarWithHoldingsTests() {
+        runFeatureTest("oaipmh-q3-marc_withholdings");
+    }
+
+    @Test
+    void oaiPmhSetsTests() {
+        runFeatureTest("sets");
+    }
+
+    @Test
+    void loadDefaultConfigurationTests() {
+        runFeature("classpath:firebird/mod-configuration/eureka/load-default-pmh-configuration.feature");
     }
 }
