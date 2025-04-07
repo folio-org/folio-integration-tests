323328d74f7a175b3eeb4e2e87317ab021244e11
TolKing1 <tolking0101@gmail.com>
2025-04-07 16:10:11 +0500

    temp: disable tests

--- mod-event-config/src/test/java/org/folio/ModEventConfigEurekaTests.java
+++ mod-event-config/src/test/java/org/folio/ModEventConfigEurekaTests.java
@@ -6,9 +6,11 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "volaris", module = "mod-event-config")
+@Disabled("REMOVE AFTER THE TESTS")
 public class ModEventConfigEurekaTests extends TestBaseEureka {
   private static final String TEST_BASE_PATH = "classpath:vega/mod-event-config/eureka-features/";
 
