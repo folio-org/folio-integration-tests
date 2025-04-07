ce8d0cd5354257041693699265b0c6f6a0b00cd6
alijonoff <azimjonalijonoff@gmail.com>
2025-04-07 12:56:33 +0500

    disabling other modules

--- mod-notify/src/test/java/org/folio/ModNotifyEurekaTests.java
+++ mod-notify/src/test/java/org/folio/ModNotifyEurekaTests.java
@@ -7,7 +7,8 @@ import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Disabled;
+@Disabled
 @FolioTest(team = "volaris", module = "mod-notify")
 public class ModNotifyEurekaTests extends TestBaseEureka {
   private static final String TEST_BASE_PATH = "classpath:vega/mod-notify/eureka-features/";
