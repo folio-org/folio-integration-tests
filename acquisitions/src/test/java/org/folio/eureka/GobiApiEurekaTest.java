07c056f9a8e633747b78052e7e664a5fa349a71c
Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>
2025-04-07 16:54:11 +0500

    FAT-17686: disable other modules tests

--- acquisitions/src/test/java/org/folio/eureka/GobiApiEurekaTest.java
+++ acquisitions/src/test/java/org/folio/eureka/GobiApiEurekaTest.java
@@ -4,8 +4,10 @@ import org.folio.test.TestBaseEureka;
 import org.folio.test.annotation.FolioTest;
 import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+@Disabled
 @FolioTest(team = "thunderjet", module = "mod-gobi")
 public class GobiApiEurekaTest extends TestBaseEureka {
 
--- acquisitions/src/test/java/org/folio/eureka/OrdersApiEurekaTest.java
+++ acquisitions/src/test/java/org/folio/eureka/OrdersApiEurekaTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+@Disabled
 @FolioTest(team = "thunderjet", module = "mod-orders")
 public class OrdersApiEurekaTest extends TestBaseEureka {
 
