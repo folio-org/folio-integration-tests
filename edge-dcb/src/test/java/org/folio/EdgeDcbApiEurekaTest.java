26f5ccc3e7ef4473117c4f23033826ebb7855954
TolKing1 <tolking0101@gmail.com>
2025-04-09 16:42:02 +0500

    fix: uncomment

--- edge-dcb/src/test/java/org/folio/EdgeDcbApiEurekaTest.java
+++ edge-dcb/src/test/java/org/folio/EdgeDcbApiEurekaTest.java
@@ -4,6 +4,8 @@ import org.folio.test.TestBaseEureka;
 import org.folio.test.annotation.FolioTest;
 import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "volaris", module = "edge-dcb")
@@ -17,27 +19,27 @@ class EdgeDcbApiEurekaTest extends TestBaseEureka {
     void testLendingFlow() {
         runFeatureTest("lending-flow-proxy.feature");
     }
-//    @Test
-//    void testBorrowingPickupFlow() {
-//        runFeatureTest("borrowing-pickup-proxy.feature");
-//    }
-//
-//    @Test
-//    void testBorrowingFlow() {
-//        runFeatureTest("borrowing-flow-proxy.feature");
-//    }
-//
-//    @Test
-//    void testPickupFlow() { runFeatureTest("pickup-flow-proxy.feature"); }
-//    @BeforeAll
-//    public void setup() {
-//        runFeature("classpath:volaris/edge-dcb/edge-dcb-junit-eureka.feature");
-//    }
+    @Test
+    void testBorrowingPickupFlow() {
+        runFeatureTest("borrowing-pickup-proxy.feature");
+    }
 
-//    @AfterAll
-//    public void tearDown() {
-//        runFeature("classpath:common/eureka/destroy-data.feature");
-//    }
+    @Test
+    void testBorrowingFlow() {
+        runFeatureTest("borrowing-flow-proxy.feature");
+    }
+
+    @Test
+    void testPickupFlow() { runFeatureTest("pickup-flow-proxy.feature"); }
+    @BeforeAll
+    public void setup() {
+        runFeature("classpath:volaris/edge-dcb/edge-dcb-junit-eureka.feature");
+    }
+
+    @AfterAll
+    public void tearDown() {
+        runFeature("classpath:common/eureka/destroy-data.feature");
+    }
 
 
 }
