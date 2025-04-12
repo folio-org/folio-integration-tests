613598b74477da2f13254c0a8ecfdeeb759cf5f5
tymofii_turtsevych <tymofii_turtsevych@epam.com>
2025-04-12 13:21:46 +0300

    Disabling tests

--- mod-audit/src/test/java/org/folio/ModAuditApiTest.java
+++ mod-audit/src/test/java/org/folio/ModAuditApiTest.java
@@ -6,9 +6,11 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "volaris", module = "mod-audit")
+@Disabled
 public class ModAuditApiTest extends TestBaseEureka {
   private static final String TEST_BASE_PATH = "classpath:firebird/mod-audit/features/";
 
