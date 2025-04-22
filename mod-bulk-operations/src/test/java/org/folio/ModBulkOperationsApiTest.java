2c3c056ba73df6ed229b6c6bce4899a89178151a
viktor-draban <Viktor_Draban@epam.com>
2025-04-22 19:48:41 +0300

    remove unused import and '*'

--- mod-bulk-operations/src/test/java/org/folio/ModBulkOperationsApiTest.java
+++ mod-bulk-operations/src/test/java/org/folio/ModBulkOperationsApiTest.java
@@ -6,7 +6,6 @@ import org.folio.test.config.TestModuleConfiguration;
 import org.folio.test.services.TestIntegrationService;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 @FolioTest(team = "firebird", module = "mod-bulk-operations")
