075ed2d1cd310ee29ebc23876449ec6de5da14da
epam-avramenko <Vasyl_Avramenko@epam.com>
2025-06-17 14:12:50 +0300

    Improve test setup and teardown logic in ModUsersTests to handle failures gracefully

--- mod-users/src/test/java/org/folio/ModUsersTests.java
+++ mod-users/src/test/java/org/folio/ModUsersTests.java
@@ -11,17 +11,30 @@ import org.junit.jupiter.api.Test;
 @FolioTest(team = "volaris", module = "mod-users")
 class ModUsersTests extends TestBaseEureka {
 
+  private boolean setupSuccessful = false;
+
   public ModUsersTests() {
     super(new TestIntegrationService(new TestModuleConfiguration("classpath:volaris/mod-users/features/")));
   }
 
   @BeforeAll
   public void setup() {
-    runFeature("classpath:volaris/mod-users/users-junit.feature");
+    try {
+      runFeature("classpath:volaris/mod-users/users-junit.feature");
+      setupSuccessful = true;
+    } catch (RuntimeException e) {
+      System.err.println("=== setup failed, skipping all tests & teardown ===");
+      throw e;
+    }
   }
 
   @AfterAll
   public void tearDown() {
+    if (!setupSuccessful) {
+      System.out.println("==== Skipping destroy-data because setup never succeeded. ====");
+      return;
+    }
+
     runFeature("classpath:common/eureka/destroy-data.feature");
   }
 
