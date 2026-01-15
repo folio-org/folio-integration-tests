bd68e655da8f7945892fa10d1597a59c0bfc7b07
Boburbek Kadirkhodjaev <b.kadirkhodjaev@gmail.com>
2026-01-15 17:36:47 +0500

    [MODORDERS-1395]. Populate holdings relations details with information about PO Lines (#2161)
    * [MODORDERS-1395]. Populate holdings relations details with information about PO Lines
    
    * [MODORDERS-1395]. Improve tenant cleanup
    
    * [MODORDERS-1395]. Refactor
    
    * [MODORDERS-1395]. Revert invalid conditionals
    
    * [MODORDERS-1395]. Update isIndividualRunMode to account for different modes of operation
    
    * [MODORDERS-1395]. Simplify assertion

--- acquisitions/src/test/java/org/folio/InvoicesSmokeApiTest.java
+++ acquisitions/src/test/java/org/folio/InvoicesSmokeApiTest.java
@@ -22,7 +22,6 @@ public class InvoicesSmokeApiTest extends TestBaseEureka {
   private static final String TEST_TENANT = "testinvoices";
   private static final int THREAD_COUNT = 4;
 
-
   private enum Feature implements org.folio.test.config.CommonFeature {
     FEATURE_1("pay-invoice-with-0-value", true);
 
