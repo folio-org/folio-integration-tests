d5dd1b41f772d66342c60e9b04ab8628e5638fdc
Kriti Jain <kjain@ebsco.com>
2026-01-26 14:25:35 -0600

    MODEXP-894: locking unlocking mapping profile

--- mod-data-export/src/test/java/org/folio/ModDataExportApiTest.java
+++ mod-data-export/src/test/java/org/folio/ModDataExportApiTest.java
@@ -50,129 +50,135 @@ public class ModDataExportApiTest extends TestBaseEureka {
         runFeatureTest("job-profiles");
     }
 
+//    @Test
+//    @Order(3)
+//    void fileUploadAndExportTest() {
+//        runFeatureTest("export");
+//    }
+//
+//    @Test
+//    @Order(4)
+//    void fileUploadAndExportWithSuppressTest() {
+//        runFeatureTest("suppress");
+//    }
+//
+//    @Test
+//    @Order(6)
+//    void fileUploadAndExportForCqlTest() {
+//        runFeatureTest("export-for-cql");
+//    }
+//
+//    @Test
+//    @Order(5)
+//    void fileExportForMarcHoldingRecordExportTest() {
+//        runFeatureTest("export-for-holdings");
+//    }
+//
+//    @Test
+//    @Order(13)
+//    void fileExportForMarcInstanceRecordExportTest() {
+//        runFeatureTest("export-for-instances-default-mapping");
+//    }
+//
+//    @Test
+//    @Order(14)
+//    void fileExportForDefaultHoldingRecordExportTest() {
+//        runFeatureTest("export-for-holdings-default-mapping");
+//    }
+//
+//    @Test
+//    @Order(8)
+//    void fileExportForMarcAuthorityRecordExportTest() {
+//        runFeatureTest("export-for-authority");
+//    }
+//
+//    @Test
+//    @Order(9)
+//    void deleteJobExecutionTest() {
+//        runFeatureTest("delete-job-execution");
+//    }
+//
+//    @Test
+//    @Order(10)
+//    void authUpdateHeadingsExportTest() {
+//        runFeatureTest("export-auth-update-headings");
+//    }
+//
+//    @Test
+//    @Order(11)
+//    void dataExportWithConsortiaTest() {
+//        runFeatureTest("consortia-export");
+//    }
+//
+//    @Test
+//    @Order(12)
+//    void dataExportDeletedMarcIdsTest() {
+//        runFeatureTest("export-deleted-marc-ids");
+//    }
+//
+//    @Test
+//    @Order(15)
+//    void dataExportDeletedAuthoritiesTest() {
+//        runFeatureTest("export-deleted-authorities");
+//    }
+//
+//    @Test
+//    @Order(16)
+//    void ExportInstanceConfigC431148Test() {
+//        runFeatureTest("export-for-instance-config-C431148");
+//    }
+//
+//    @Test
+//    @Order(17)
+//    void ExportInstanceConfigCqlC432309Test() {
+//        runFeatureTest("export-for-instance-config-cql-C432309");
+//    }
+//
+//    @Test
+//    @Order(18)
+//    void ExportHoldingConfigC432311Test() {
+//        runFeatureTest("export-for-holding-config-C432311");
+//    }
+//
+//    @Test
+//    @Order(19)
+//    void ExportAuthorityConfigC432314Test() {
+//        runFeatureTest("export-authority-config-C432314");
+//    }
+//
+//    @Test
+//    @Order(20)
+//    void NegativeConfigExportC432315Test() {
+//        runFeatureTest("negative-config-export-C432315");
+//    }
+//
+//    @Test
+//    @Order(21)
+//    void dataExportDeletedAuthoritiesDeletedProfileTest() {
+//        runFeatureTest("export-deleted-authorities-deleted-profile");
+//    }
+//
+//    @Test
+//    @Order(22)
+//    void linkedDataExportTest() {
+//        runFeatureTest("export-for-linked-data");
+//    }
+//
+//    @Test
+//    @Order(23)
+//    void exportDeletedJobProfileTest() {
+//        runFeatureTest("export-deleted-job-profile");
+//    }
+//
+//    @Test
+//    @Order(24)
+//    void exportLockedJobProfileTest() {
+//        runFeatureTest("export-locked-job-profile");
+//    }
+
     @Test
     @Order(3)
-    void fileUploadAndExportTest() {
-        runFeatureTest("export");
-    }
-
-    @Test
-    @Order(4)
-    void fileUploadAndExportWithSuppressTest() {
-        runFeatureTest("suppress");
-    }
-
-    @Test
-    @Order(6)
-    void fileUploadAndExportForCqlTest() {
-        runFeatureTest("export-for-cql");
-    }
-
-    @Test
-    @Order(5)
-    void fileExportForMarcHoldingRecordExportTest() {
-        runFeatureTest("export-for-holdings");
-    }
-
-    @Test
-    @Order(13)
-    void fileExportForMarcInstanceRecordExportTest() {
-        runFeatureTest("export-for-instances-default-mapping");
-    }
-
-    @Test
-    @Order(14)
-    void fileExportForDefaultHoldingRecordExportTest() {
-        runFeatureTest("export-for-holdings-default-mapping");
-    }
-
-    @Test
-    @Order(8)
-    void fileExportForMarcAuthorityRecordExportTest() {
-        runFeatureTest("export-for-authority");
-    }
-
-    @Test
-    @Order(9)
-    void deleteJobExecutionTest() {
-        runFeatureTest("delete-job-execution");
-    }
-
-    @Test
-    @Order(10)
-    void authUpdateHeadingsExportTest() {
-        runFeatureTest("export-auth-update-headings");
-    }
-
-    @Test
-    @Order(11)
-    void dataExportWithConsortiaTest() {
-        runFeatureTest("consortia-export");
-    }
-
-    @Test
-    @Order(12)
-    void dataExportDeletedMarcIdsTest() {
-        runFeatureTest("export-deleted-marc-ids");
-    }
-
-    @Test
-    @Order(15)
-    void dataExportDeletedAuthoritiesTest() {
-        runFeatureTest("export-deleted-authorities");
-    }
-
-    @Test
-    @Order(16)
-    void ExportInstanceConfigC431148Test() {
-        runFeatureTest("export-for-instance-config-C431148");
-    }
-
-    @Test
-    @Order(17)
-    void ExportInstanceConfigCqlC432309Test() {
-        runFeatureTest("export-for-instance-config-cql-C432309");
-    }
-
-    @Test
-    @Order(18)
-    void ExportHoldingConfigC432311Test() {
-        runFeatureTest("export-for-holding-config-C432311");
-    }
-
-    @Test
-    @Order(19)
-    void ExportAuthorityConfigC432314Test() {
-        runFeatureTest("export-authority-config-C432314");
-    }
-
-    @Test
-    @Order(20)
-    void NegativeConfigExportC432315Test() {
-        runFeatureTest("negative-config-export-C432315");
-    }
-
-    @Test
-    @Order(21)
-    void dataExportDeletedAuthoritiesDeletedProfileTest() {
-        runFeatureTest("export-deleted-authorities-deleted-profile");
-    }
-
-    @Test
-    @Order(22)
-    void linkedDataExportTest() {
-        runFeatureTest("export-for-linked-data");
-    }
-
-    @Test
-    @Order(23)
-    void exportDeletedJobProfileTest() {
-        runFeatureTest("export-deleted-job-profile");
-    }
-
-    @Test
-    @Order(24)
-    void exportLockedJobProfileTest() {
-        runFeatureTest("export-locked-job-profile");
+    void exportLockedMappingProfileTest() {
+        runFeatureTest("export-locked-mapping-profile");
     }
 }
