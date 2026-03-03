f49f1e045e17e3345b79e2ef1669fa632a1bb59c
pkjacob <121239864+pkjacob@users.noreply.github.com>
2026-03-03 08:31:07 -0500

    Import new Instance connected to an existing Work (#2219)
    * Import new Instance connected to an existing Work
    
    * Fix typo in assert

--- mod-linked-data/src/test/java/org/folio/ModLinkedDataTest.java
+++ mod-linked-data/src/test/java/org/folio/ModLinkedDataTest.java
@@ -70,6 +70,11 @@ class ModLinkedDataTest extends TestBaseEureka {
   }
 
   @Test
+  void importInstanceConnectedToExistingWork() {
+    runFeatureTest("import-rdf/new-instance-of-existing-work.feature");
+  }
+
+  @Test
   void rdfBulkImport() {
     runFeatureTest("import-rdf-bulk/import-rdf-bulk.feature");
   }
